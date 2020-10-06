2d4e15e5bc7411fa95e307f0f5c3c07d7f20e8a0
Angel <v.vprather@gmail.com>
2020-10-06 10:14:44 -0500

    what i have so far

---
+++ main.js
@@ -0,0 +1,56 @@
+// console.log("idk what im doing")
+
+// let numrolls = document.querySelector ("submitButton");
+// let userinput = document.querySelector ("numinput");
+// let totroll = document.querySelector (".totroll");
+
+// let Roll = rollbutton
+
+let maxrollsInput =document.querySelector('numinput');
+let rollButton = document.querySelector('submitButton')
+
+console.log(maxrollsInput.value)
+
+let rollsArray = []
+let maxRolls =6;
+
+let count = 0;
+
+submitButton.addEventListener("click", function() {
+    console.log("thankyou Chris")
+
+})
+
+
+
+
+
+
+    while (count <= 6) {
+        //run this
+        console.log(count);
+        count ++
+    }
+
+
+    showall. //and now nothing works again {}
+
+
+//     while (count < userinput) {
+//         //IDK//
+        
+//         if (count <= userinput) {
+//             ///THEN IDK WHAT TO EVEN WRITE // ------>
+//         }
+//         count ++
+//     }
+
+
+// Roll.addEventListener("click"), function (){
+    
+// }
+
+// let Dice1 =[1, 2, 3, 4, 5, 6]
+// let Dice2 =[1, 2, 3, 4, 5, 6]
+
+// //I DON'T UNDERSTAND ANY OF THIS ANYMORE :,( ///////////cheese...
